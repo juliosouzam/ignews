418763e61665fe7bb2a39721d910d22956cb06d2
Júlio César <julio.souzam@gmail.com>
2021-03-31 22:13:10 -0300

    :tada: ignews completed

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,73 @@
+import NextAuth, { Session } from 'next-auth';
+import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
+import { WithAdditionalParams } from 'next-auth/_utils';
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: 'read:user',
+    }),
+    // ...add more providers here
+  ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async session(session: Session): Promise<WithAdditionalParams<Session>> {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ])
+          )
+        );
+
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch (error) {
+        return { ...session, activeSubscription: null };
+      }
+    },
+    async signIn(user) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
